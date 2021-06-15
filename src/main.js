2c4c2e089d12cb82e5ad3f6faa684f6fae40cc86
Muhammad <yakhshiboev@bk.ru>
2021-06-15 15:59:34 +0500

    v1.0.0

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import Paginate from 'vuejs-paginate';
+import {store} from "./store/store";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.component('Paginate', Paginate);
+
 
 Vue.config.productionTip = false;
 
